c964c13b27b856a2027db51e2da462fe585a10a1
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-11-24 16:31:51 +0200

    test

--- src/main/java/com/skinDreams/School/controllers/HealthCheckController.java
+++ src/main/java/com/skinDreams/School/controllers/HealthCheckController.java
@@ -1,2 +1,12 @@
-package com.skinDreams.School.controllers;public class HealthCheckController {
+package com.skinDreams.School.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
 }
